1bb2bfa4773b6acc2256e7f6aeb5e47743a637f5
Mark Johnson <mark124.mj@gmail.com>
2018-12-05 10:48:39 -0500

    Create FirstProgram.java

---
+++ Activity 1-1/FirstProgram.java
@@ -0,0 +1,14 @@
+// First program in Java
+// Mark Smith Johnson
+
+public class FirstProgram
+{
+  public static void main(String [] args)
+  {
+    System.out.println("Programming is not a spectator sport!");
+    // Student code starts here:
+    System.out.println("Mark Smith Johnson is one of the players");
+    // Student code ends here.
+    System.exit(0);
+  }
+}
